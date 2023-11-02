58b3c8819cf17076b9f046c738e92ddbf11aac8b
Sayan Biswas <dankguy6146@gmail.com>
2023-11-02 13:05:07 +0000

    fix: issues with build due to usage of "fs"

--- apps/squares-docs/src/app/foundations/colors/page.tsx
+++ apps/squares-docs/src/app/foundations/colors/page.tsx
@@ -17,7 +17,7 @@ import {
   Tabs,
 } from '@cubik/ui';
 
-import { serializeSemantic } from '@cubik/helper-scripts';
+import { serializeSemantic } from '@cubik/helper-scripts/src/color/serializeSemantic';
 import PageHOC from '../../home-page-components/components/pageHOC';
 
 type objectType = {
