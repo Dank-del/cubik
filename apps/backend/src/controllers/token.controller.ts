2de68881bf1a9e6b556a1f7a01b122ef0797f2a1
Sayan Biswas <me@sayanbiswas.in>
2023-11-13 22:54:15 +0530

    refactor: remove unused code and add new price route
    Signed-off-by: Sayan Biswas <me@sayanbiswas.in>

--- apps/backend/src/controllers/token.controller.ts
+++ apps/backend/src/controllers/token.controller.ts
@@ -3,10 +3,15 @@ import { tokenPrice } from 'utils/price';
 
 export const tokenPriceController = async (req: Request, res: Response) => {
   try {
-    const token = req.params.token;
-    const price = await tokenPrice(token);
+    const { token } = req.params;
+    const data = await tokenPrice(token);
 
-    res.status(200).json({ price: parseFloat(price?.toFixed(3) || '0.0') });
+    res.status(200).json({
+      token: data?.mintSymbol,
+      name: '',
+      price: data?.price,
+      icon: '',
+    });
   } catch (error) {
     console.error(error);
     return res.status(500).json({ error: error });
@@ -14,16 +19,16 @@ export const tokenPriceController = async (req: Request, res: Response) => {
 };
 export const tokenPriceMultiple = async (req: Request, res: Response) => {
   try {
-    const tokens = req.body.tokens as string[];
+    const tokens = ['SOL', 'USDC', 'ETH'];
 
     const prices = await Promise.all(
       tokens.map(async (token: string) => {
-        const price = await tokenPrice(token);
-        return { token: token, price: parseFloat(price?.toFixed(3) || '0.0') };
+        const data = await tokenPrice(token);
+        return { token: token, price: data?.price, name: '', icon: '' };
       }),
     );
 
-    res.status(200).json({ prices: prices });
+    res.status(200).json({ result: prices });
   } catch (error) {
     console.error(error);
     return res.status(500).json({ error: error });
